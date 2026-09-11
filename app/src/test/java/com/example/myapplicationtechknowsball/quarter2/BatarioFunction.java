b9bd860cd23a7b7626570a95512efe44bebdcbf8
peter-batario-ict <peter.batario1@gmail.com>
2026-09-11 19:12:08 +0800

    Added BatarioFunction

---
+++ app/src/test/java/com/example/myapplicationtechknowsball/quarter2/BatarioFunction.java
@@ -0,0 +1,33 @@
+package com.example.myapplicationtechknowsball.quarter2;
+
+import org.junit.Test;
+
+public class BatarioFunction {
+
+    @Test
+    public void printMyProfile() {
+
+        // Personal details
+        String myName = "Peter";
+        String petName = "Adie";
+        String favFood = "Roasted Beef";
+        int myAge = 16;
+
+        // Display profile
+        System.out.println("===== MY DIGITAL PROFILE =====");
+
+        System.out.println(
+                "Hello, my name is " + myName +
+                        " and I am " + myAge + " years old."
+        );
+
+        System.out.println(
+                "I have a wonderful pet named " + petName + "."
+        );
+
+        System.out.println(
+                "If I could, I would eat " + favFood +
+                        " every single day!"
+        );
+    }
+}
