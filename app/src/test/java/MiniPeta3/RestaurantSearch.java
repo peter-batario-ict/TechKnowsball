b71f738622061c882bb2f0821119ddcb63d118c4
Aiyesha1028 <aiyeshanomura12@gmail.com>
2026-09-23 13:14:22 +0800

    fixed

--- app/src/test/java/MiniPeta3/RestaurantSearch.java
+++ app/src/test/java/MiniPeta3/RestaurantSearch.java
@@ -1,4 +1,42 @@
 package MiniPeta3;
 
-public class RestaurantSearch {
+    import java.util.Scanner;
+
+    public class RestaurantSearch {
+
+        public void search(Scanner scanner) {
+
+            String[] restaurants = {
+                    "Jollibee",
+                    "McDonald's",
+                    "Mang Inasal",
+                    "Chowking",
+                    "Shakey's",
+                    "Greenwich",
+                    "Pizza Hut",
+                    "KFC"
+            };
+
+            System.out.println();
+            System.out.println("===== RESTAURANT SEARCH =====");
+            System.out.print("Enter restaurant name: ");
+
+            String search = scanner.nextLine().trim().toLowerCase();
+
+            boolean found = false;
+
+            for (String restaurant : restaurants) {
+
+                if (restaurant.toLowerCase().contains(search)) {
+                    System.out.println("Restaurant found: " + restaurant);
+                    found = true;
+                }
+            }
+
+            if (!found) {
+                System.out.println("No restaurant found.");
+            }
+
+            System.out.println("==============================");
+        }
 }
