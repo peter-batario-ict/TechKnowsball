2509a617d2402b7c8b02ef6a2d11706c39f6144e
Narvaja06 <narvajaprince05@gmail.com>
2026-09-26 23:31:37 +0800

    1st commit (skeleton)
    -added buy snacks and tickets

--- app/src/test/java/quarter2/exampractical/CinemaTicketing/CinemaTicketingMenu.java
+++ app/src/test/java/quarter2/exampractical/CinemaTicketing/CinemaTicketingMenu.java
@@ -5,5 +5,18 @@ import java.util.Scanner;
 public class CinemaTicketingMenu {
 
     public void start(Scanner scanner) {
+
+        boolean running = true;
+
+        while (running) {
+
+            System.out.println("\n==============================");
+            System.out.println("       CINEMA TICKETING");
+            System.out.println("==============================");
+            System.out.println("1. Buy Ticket");
+            System.out.println("2. Buy Snacks");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+        }
     }
 }
